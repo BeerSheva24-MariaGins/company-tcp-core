e27a53d2934b8d7d07b208e4563d23b999bef4dd
MariaGins <m.gins@mail.ru>
2024-11-20 21:00:33 +0200

    New version with addition class

--- company-server-application/src/main/java/telran/employees/Main.java
+++ company-server-application/src/main/java/telran/employees/Main.java
@@ -1,38 +1,24 @@
 package telran.employees;
 
 import telran.io.Persistable;
+import telran.io.PersistableSaverThread;
 import telran.net.TcpServer;
 
 public class Main {
     private static final String FILE_NAME = "employees.data";
-    private static final int PORT = 4000;
-    private static final long DEFAULT_INTERVAL_SEC = 10; 
-    private static long saveInterval = DEFAULT_INTERVAL_SEC;
-
-    public static void main(String[] args) {
-        Company company = new CompanyImpl();
-        
-        if (company instanceof Persistable persistable) {
-            persistable.restoreFromFile(FILE_NAME);
-            
-            Thread saveThread = new Thread(() -> {
-                while (true) {
-                    try {
-                        Thread.sleep(saveInterval * 1000);
-                        persistable.saveToFile(FILE_NAME);                        
-                    } catch (InterruptedException e) {
-                        System.out.println("Saving is finished");
-                        break;
-                    } catch (Exception e) {
-                        System.err.println("Error: " + e.getMessage());
-                    }
-                }
-            });
-            saveThread.setDaemon(true); 
-            saveThread.start();
-        }
-
-        TcpServer tcpServer = new TcpServer(new CompanyProtocol(company), PORT);
-        tcpServer.run();
+        private static final int PORT = 4000;
+                private static final long TIME_INTERVAL = 60000;
+                
+                    public static void main(String[] args) {
+                        Company company = new CompanyImpl();
+                        if (company instanceof Persistable persistable) {
+                            persistable.restoreFromFile(FILE_NAME);
+                            PersistableSaverThread saverThread = new PersistableSaverThread(persistable, FILE_NAME, TIME_INTERVAL);
+                            saverThread.start();
+                            Runtime.getRuntime().addShutdownHook(new Thread(() -> persistable.saveToFile(FILE_NAME)));
+            }
+           
+            TcpServer tcpServer = new TcpServer(new CompanyProtocol(company), PORT);
+            tcpServer.run();
     }
 }
